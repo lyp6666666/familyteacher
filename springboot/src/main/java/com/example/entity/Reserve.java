a3f84c622cd32947b03652f7e3889a2d39a4c5fa
Lu Yipeng <532567840@qq.com>
2024-05-27 12:50:05 +0800

    second

---
+++ springboot/src/main/java/com/example/entity/Reserve.java
@@ -0,0 +1,65 @@
+package com.example.entity;
+
+import java.io.Serializable;
+
+/**
+ * 家教预约表
+*/
+public class Reserve implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private Integer userId;
+    private Integer teacherId;
+    private String start;
+    private String end;
+    private String content;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(Integer teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ vue/src/views/front/TeacherDetail.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="main-content">
+    <div style="width: 60%; margin: 80px auto">
+      <div style="display: flex">
+        <div style="width: 200px; text-align: center">
+          <img :src="infoData.teacherAvatar" alt="" style="width: 200px; height: 200px; border-radius: 50%">
+          <div style="margin: 20px 0; font-size: 16px">
+            <strong>姓名：</strong> {{infoData.teacherName}}
+          </div>
+          <div>
+            <el-rate v-model="infoData.star" disabled></el-rate>
+          </div>
+        </div>
+        <div style="width: 250px; margin-left: 50px">
+          <div style="height: 40px; line-height: 40px; font-size: 16px">
+            <strong>学校：</strong><span style="color: #666666">{{infoData.school}}</span>
+          </div>
+          <div style="height: 40px; line-height: 40px; font-size: 16px">
+            <strong>学历：</strong><span style="color: #666666">{{infoData.education}}</span>
+          </div>
+          <div style="height: 40px; line-height: 40px; font-size: 16px">
+            <strong>专业：</strong><span style="color: #666666">{{infoData.speciality}}</span>
+          </div>
+          <div style="height: 40px; line-height: 40px; font-size: 16px">
+            <strong>等级：</strong><span style="color: #666666">{{infoData.level}}</span>
+          </div>
+          <div style="height: 40px; line-height: 40px; font-size: 16px">
+            <strong>生日：</strong><span style="color: #666666">{{infoData.birth}}</span>
+          </div>
+          <div style="height: 40px; line-height: 40px; font-size: 16px">
+            <strong>邮箱：</strong><span style="color: #666666">{{infoData.teacherEmail}}</span>
+          </div>
+          <div style="height: 40px; line-height: 40px; font-size: 16px">
+            <strong>电话：</strong><span style="color: #666666">{{infoData.teacherPhone}}</span>
+          </div>
+        </div>
+        <div style="flex: 1; margin-left: 20px">
+          <div style="height: 40px; line-height: 40px; font-size: 16px">
+            <strong>介绍：</strong><span style="color: #666666">{{infoData.description}}</span>
+          </div>
+          <div style="height: 40px; line-height: 40px; font-size: 16px">
+            <strong>说明：</strong><span style="color: #666666">{{infoData.plan}}</span>
+          </div>
+          <div style="height: 40px; line-height: 40px; font-size: 16px">
+            <strong>省份：</strong><span style="color: #666666">{{infoData.province}}</span>
+          </div>
+          <div style="height: 40px; line-height: 40px; font-size: 16px">
+            <strong>地址：</strong><span style="color: #666666">{{infoData.address}}</span>
+          </div>
+          <div style="height: 40px; line-height: 40px; font-size: 16px">
+            <strong>家教：</strong><span style="color: #666666">{{infoData.typeName}}</span>
+          </div>
+          <div style="height: 40px; line-height: 40px; font-size: 16px">
+            <strong>形式：</strong><span style="color: #666666">{{infoData.type}}</span>
+          </div>
+        </div>
+      </div>
+      <div style="margin-top: 40px; text-align: center">
+        <el-button type="primary" style="padding: 15px 40px">预约家教</el-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: JSON.parse(localStorage.getItem('xm-user') || '{}'),
+      infoId: this.$route.query.id,
+      infoData: {}
+    }
+  },
+  created() {
+    this.loadInfo()
+  },
+  methods: {
+    loadInfo() {
+      this.$request.get('/info/selectById/' + this.infoId).then(res => {
+        if (res.code === '200') {
+          this.infoData = res.data
+        } else {
+          this.$message.error(res.msg)
+        }
+      })
+    }
+  }
+}
+</script>
